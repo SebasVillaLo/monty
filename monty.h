bea686ada235ae7ff685c61b4fe3e802c2525b3e
SebasVillaLo <villalopezsebastian@gmail.com>
2021-09-21 14:50:42 -0500

    update files

--- monty.h
+++ monty.h
@@ -32,9 +32,9 @@ var_t var;
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 /**
  * struct instruction_s - opcode and its function
@@ -46,8 +46,8 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 /*get_funs.c*/
