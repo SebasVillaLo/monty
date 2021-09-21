fa0a4feb0410bf38393acfe3a50f08e01e392829
Christian Diaz <84691244+TheMasterTz@users.noreply.github.com>
2021-09-21 16:49:38 -0500

    Update pint.c

--- pint.c
+++ pint.c
@@ -3,9 +3,11 @@
 void _pint(stack_t **stack, unsigned int line_number)
 {
 	stack_t *head = *stack;
+
 	if (var.stack_len == 0)
 	{
-		
+		printf("L%u: can't pint, stack empty", line_number);
+		exit(EXIT_FAILURE);
 	}
 
 	printf("%d\n", head->n);
