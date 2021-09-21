477cf6c4368984af82c7a924763974495a74cbfb
SebasVillaLo <villalopezsebastian@gmail.com>
2021-09-21 16:54:46 -0500

    add pint

--- pint.c
+++ pint.c
@@ -1,11 +1,18 @@
 #include "monty.h"
-
+/**
+ * _pint - print the information for node
+ * @stack: ...
+ * @line_number: ...
+ */
 void _pint(stack_t **stack, unsigned int line_number)
 {
+	var_t var;
 	stack_t *head = *stack;
+
 	if (var.stack_len == 0)
 	{
-		
+		printf("L%u: can't pint, stack empty", line_number);
+		exit(EXIT_FAILURE);
 	}
 
 	printf("%d\n", head->n);
