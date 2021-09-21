277a169e144fa31ffd06310a4dcdafa7ef8d552e
Christian Diaz <84691244+TheMasterTz@users.noreply.github.com>
2021-09-21 15:34:03 -0500

    Create add_node.c

---
+++ add_node.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+
+stack_t *add_node(stack_t **stack, const int n)
+{
+	stack_t *NewNode;
+
+	if (stack == NULL)
+		return (NULL);
+	
+	NewNode = malloc(sizeof(stack_t));
+	if (NewNode == NULL)
+		return (NULL);
+	
+	NewNode->n = n;
+	if (*stack == NULL)
+	{
+		NewNode->prev = NewNode;
+		NewNode->next = NewNode;
+	}
+	else
+	{
+		(*stack)->prev->next = NewNode;
+		NewNode->prev = (*stack)->prev;
+		(*stack)->prev = NewNode;
+		NewNode->next = *stack;
+	}
+	*stack = NewNode;
+	return (NewNode);
+}
